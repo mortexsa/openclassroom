f8369139fa3e47c0eda5b80477b0b29a514b8ee9
mortexsa <slimani-arezki@hotmail.com>
2017-10-11 10:08:05 +0200

    petit truc pour tester le javascript mais sans plus , en plus ca ne marche meme pas donc bon , sa serre un peut a rien ce que jai fait dans les deux fichier que je vien de rajouter.

---
+++ bouffer.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>JavaScript</title>
+</head>
+<body>
+	<script language="JavaScript" >
+		function doSomething() {
+  			console.log(window.open('https://www.instagram.com/r1.fitness/', '_self'));
+		}
+
+		setInterval(doSomething, 2000);
+	</script>
+</body>
+</html>
